4b46e55f98a4f8bda755c47f8aa461235b46c39a
ademnr <ademnr2@gmail.com>
2025-07-21 20:22:41 +0100

    clock logo removed

--- components/PreviewLoadingComponent.tsx
+++ components/PreviewLoadingComponent.tsx
@@ -192,9 +192,7 @@ const LoadingComponent: React.FC<LoadingComponentProps> = ({
                     {/* Tips while loading */}
                     <div className="text-center">
                         <div className="inline-flex items-center space-x-2 bg-slate-700/30 border border-slate-600/50 rounded-lg px-4 py-3">
-                            <svg className="w-5 h-5 text-cyan-400" fill="currentColor" viewBox="0 0 20 20">
-                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd" />
-                            </svg>
+
                             <span className="text-slate-300 text-sm">
                                 {tips[currentTip]}
                             </span>
